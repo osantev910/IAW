a11a3efbc0a355239cb3a30c7dbd04713e054ad7
osantev910 <146702528+osantev910@users.noreply.github.com>
2023-10-09 09:48:09 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        
+        /*echo permite varios parámetros
+         * seguidos por "," pero print solo permite uno 
+         */
+      echo "'Texto sencillo'" , "<h2>Título de segundo nivel</h2>" , "Texto sencillo";   
+      
+       print "Texto sencillo" ;
+       print"<h2>Título de segundo nivel</h2>" ;
+       print"Texto sencillo";
+        ?>
+    </body>
+</html>
